dd73c42b2d48281cfd9ffba13a0fd1f46327f867
Zeke <16123829+zbarge@users.noreply.github.com>
2018-06-15 21:26:33 -0500

    Working on local flask app

--- stocklook/run.py
+++ stocklook/run.py
@@ -1,8 +1,12 @@
 from flask import Flask, g, flash, abort, session, request, url_for, redirect, render_template
+import jinja2, os
 
 
-app = Flask('stocklook')
+app = Flask('stocklook',
+            #template_folder=os.path.join(os.path.dirname(__file__), 'templates')
+            )
 app.config.from_object(__name__)
+
 try:
     app.config.from_envvar('STOCKLOOK_WEB_SETTINGS', silent=False)
 except RuntimeError as e:
@@ -11,7 +15,11 @@ except RuntimeError as e:
 
 @app.route('/')
 def index():
-    return "Hello"
+    for dirname, subdirs, files in os.walk('/static'):
+        for f in files:
+            print(f)
+    print("walked static dir.")
+    return render_template('index.html')
 
 
 @app.route('/login')
